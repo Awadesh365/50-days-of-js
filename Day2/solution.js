f27c933509dc7191e403062c6efc95c996a16ee2
Awadesh <36awadesh@gmail.com>
2025-01-02 23:25:56 +0530

    Day 2 Completed.

---
+++ Day2/solution.js
@@ -0,0 +1,13 @@
+// Create your product object
+const product = {
+    id: 101,           // Numeric value for 'id'
+    name: "Laptop",    // Non-empty string for 'name'
+    price: 999.99,     // Numeric value for 'price'
+    availability: true // Boolean value for 'availability'
+};
+
+// Export your product object
+export { product };
+
+// Console.log the object to view the output
+console.log(product);
